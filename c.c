082b2b57188c1608fc15a2b5ba99d25e3d4be63f
Erik Falor <ewfalor@gmail.com>
2015-11-02 12:29:57 -0700

    C code now dynamically allocates an array (like the other langs)

--- c.c
+++ c.c
@@ -3,14 +3,23 @@
 
 int main(void) {
 	long long unsigned int lim = 999999;
+	long long unsigned int *a;
 
 	char *LIMIT = getenv("LIMIT");
 	if (LIMIT)
 		lim = (long long unsigned int)atoll(LIMIT);
 
+	if ((a = (void *)malloc(sizeof(long long unsigned int) * lim)) == NULL) {
+		perror("malloc()");
+		exit(1);
+	}
+
+	for (long long unsigned int i = 0; i <= lim; ++i)
+		a[i] = i;
+
 	long long unsigned int sum = 0;
 	for (long long unsigned int i = 0; i <= lim; ++i)
-		sum += i;
+		sum += a[i];
 
 	printf("%llu\n", sum);
 }
