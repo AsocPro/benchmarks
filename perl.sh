b4a8ab6b6bb3c5aaf40dae8f4ad034fed51694b6
chad <cwayment@spillman.com>
2015-11-05 07:48:37 -0700

    merging code changes

--- perl.sh
+++ perl.sh
@@ -6,4 +6,4 @@ LIMIT=${LIMIT:-999999}
 echo 
 echo $0: LIMIT=$LIMIT
 
-perl -MList::Util=sum -e "print sum(0..$LIMIT), qq{\\n}"
+/usr/bin/time perl -MList::Util=sum -e "print sum(0..$LIMIT), qq{\\n}"
