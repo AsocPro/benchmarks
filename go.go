ffd5ad595e5eb84dab2499143a2227cba8a4aeb1
zgibbs <zgibbs@spillman.com>
2015-11-10 12:52:51 -0700

    Finish Go

--- go.go
+++ go.go
@@ -1,12 +1,22 @@
 package main
  
 import "fmt"
+import "os"
+import "strconv"
  
 func main() {
-    sum, prod := 0, 1
-    for _, x := range []int{1,2,5} {
-        sum += x
-        prod *= x
-    }
-    fmt.Println(sum, prod)
+
+	var limit int = 999999
+	if string, err := strconv.Atoi(os.Getenv("LIMIT")); err == nil {
+		limit = string
+	}
+	var numbers = make([]int, limit+1)
+	for count := 0; count <= limit ; count++ {
+		numbers[count] = count
+	}
+	sum := 0
+	for count := 0; count <= limit ; count++ {
+		sum += numbers[count]
+	}
+	fmt.Println(sum)
 }
---
+++ go.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+LIMIT=${LIMIT:-999999}
+
+echo
+echo $0: LIMIT=$LIMIT
+
+/usr/bin/time ./go
+
