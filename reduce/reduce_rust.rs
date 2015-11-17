706be7c21295978348513f6e32dfde4f4e03ba54
Mike Killpack <mkillpack@spillman.com>
2015-11-17 14:24:39 -0700

    Use names that are easier to understand

--- reduce/reduce_rust.rs
+++ reduce/reduce_rust.rs
@@ -2,7 +2,7 @@ use std::env;
 
 #[cfg(test)]
 mod tests {
-    use super::env_to_str;
+    use super::env_to_num;
     use std::env;
 
     #[test]
@@ -10,21 +10,21 @@ mod tests {
         let key = "TESTVAR";
         let value = "42";
         env::set_var(key, value);
-        assert_eq!(env_to_str(key), Ok(42));
+        assert_eq!(env_to_num(key), Ok(42));
     }
 }
 
-fn env_to_str(var_name: &str) -> Result<u64, String> {
+fn env_to_num(var_name: &str) -> Result<u64, String> {
     let var = try!(env::var(var_name).map_err(|e| e.to_string()));
     let num: u64 = try!(var.parse::<u64>().map_err(|e| e.to_string()));
     Ok(num)
 }
 
 fn main() {
-    let to_val = match env_to_str("LIMIT") {
+    let limit = match env_to_num("LIMIT") {
         Ok(n) => n,
         Err(_) => 999999,
     };
-    let nums = (1..to_val).collect::<Vec<u64>>();
+    let nums = (1..limit).collect::<Vec<u64>>();
     println!("{}", nums.iter().fold(0, |acc, &item| acc + item));
 }
