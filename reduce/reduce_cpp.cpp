283bacc816b974d455af85c133a0e3406713f25f
Mike Killpack <mkillpack@spillman.com>
2015-12-01 12:42:18 -0700

    reduce_cpp: use vector iterator to compute sum
    This commit makes the computed sum match what is now assumed to be the
    correct value by making the value range from 0 to LIMIT inclusive.
    
    Additionally, this commit updates the way the vector of integers is
    handled. Previously, values were assigned to the vector as if it were a
    plain array. The values are now assigned via push_back() so that the
    vector's size is correct and an iterator created from the vector can
    actually iterate over the container.

--- reduce/reduce_cpp.cpp
+++ reduce/reduce_cpp.cpp
@@ -11,16 +11,16 @@ int main(){
 		numlimit = std::atoi(limit);
 	}
 	std::vector<int> a;
-	a.reserve(numlimit);	
+	a.reserve(numlimit + 1);
 
-	for(int i = 0; i < numlimit; i++){ 
-		a[i] = i;
+	for (int i = 0; i <= numlimit; i++) {
+		a.push_back(i);
 	}
 
 	long sum = 0;
 
-	for (int n = 0; n < numlimit; n++){
-	    sum += n;
+	for (std::vector<int>::iterator it = a.begin(); it != a.end(); ++it) {
+	    sum += *it;
 	}
 
 	std::cout << sum << std::endl;
